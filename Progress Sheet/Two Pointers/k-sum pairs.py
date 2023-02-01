a140c48ba080be03745942dedbf4a4e8c7c0b05c
Emnet Woldu <96937795+Emni3@users.noreply.github.com>
2023-02-01 20:04:15 +0300

    Create k-sum pairs.py

---
+++ Progress Sheet/Two Pointers/k-sum pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        first = 0
+        next = len(nums)-1
+        operations = 0
+        nums.sort()
+        while first < next:
+            if nums[first] + nums[next] > k:
+                next -= 1
+               
+            elif nums[first] + nums[next] < k:
+                first += 1
+            else:
+                first += 1
+                next -= 1
+                operations += 1
+        return operations
+        
