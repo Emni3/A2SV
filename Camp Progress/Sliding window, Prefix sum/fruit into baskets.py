f184e10f2e380313431b55a8f5eb905b7cef992d
Emnet Woldu <96937795+Emni3@users.noreply.github.com>
2023-02-23 17:15:54 +0300

    Create fruit into baskets.py

---
+++ Camp Progress/Sliding window, Prefix sum/fruit into baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        picked = 0
+        basket = defaultdict(int)
+        left = 0
+        for right in range(len(fruits)):
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+            basket[fruits[right]] += 1
+            if len(basket)<=2:
+                picked = max(right - left + 1, picked)
+           
+        return picked
+            
