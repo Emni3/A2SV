cbb2a9fc650cb86db1f08863d0681f98086ef81e
Emnet Woldu <96937795+Emni3@users.noreply.github.com>
2023-03-03 22:24:12 +0300

    Create evaluate reverse polish.py

---
+++ Camp Progress/Stack & Queue/evaluate reverse polish.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for t in tokens:
+            if t != "+" and t != "-" and t != "*" and t != "/":
+                stack.append(int(t))
+            else:
+                a = int(stack.pop())
+                b = int(stack.pop()) 
+                if t == '+':
+                    stack.append(a + b)
+                elif t == '-':
+                    stack.append(b - a)
+                elif t == '*':
+                    stack.append(a * b)
+                elif t == '/':
+                    stack.append(int(b / a))
+        
+        return stack[-1]
